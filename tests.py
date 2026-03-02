da63336d89fb4bce3d3aab4b734b37b88847d64f
m_nguyen <minh-tam1.nguyen@etu.u-pec.fr>
2026-03-02 16:39:59 +0100

    Jeu du compteur (Légère modification sur les espacements des mots)

--- tests.py
+++ tests.py
@@ -1,13 +1,20 @@
 import random
 
-m = random.randint(0, 1000)
+compte = 0
+m = random.randint(0, 100)
 reponse = -1
 
 while reponse!=m:
-    reponse = int(input("Entrez un nombre entre 0 et 1000 : "))
+    reponse = int(input("Entrez un nombre entre 0 et 100 : "))
     if reponse < m:
         print("Plus !")
+        compte += 1
+        if compte%3 == 0:
+            m = random.randint(0, 100)
     elif reponse > m:
         print("Moins !")
+        compte += 1
+        if compte%3 == 0:
+            m = random.randint(0, 100)
     else:
         print("Bravo, vous avez gagné !")
