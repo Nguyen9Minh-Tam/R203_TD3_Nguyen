3e356f1799d89c25f2cdf5779d8fc56bd5ce419a
m_nguyen <minh-tam1.nguyen@etu.u-pec.fr>
2026-03-02 16:54:07 +0100

    Ajout comptage aléatoire

--- compteur.py
+++ compteur.py
@@ -1,10 +1,15 @@
-compteur = 0
+import random
 
-while compteur !=1000:
-    valide = int(input(f"Comptons jusqu'à 1000 ! Entrez le nombre après {compteur} : "))
+compteur = 0
+nombre = random.randint(0, 1000)
+
+while compteur !=nombre:
+    valide = int(input(f"Comptons jusqu'au nombre inconnu ! Entrez le nombre après {compteur} : "))
     for _ in range(50):
         print("")
     compteur += 1
     if valide != compteur:
-        print("Recommencez ! Compteur remis à 0 !")
+        print("Recommencez ! Compteur remis à 0.")
         compteur = 0
+    else:
+        print("Bravo! Vous avez fini de compter")
